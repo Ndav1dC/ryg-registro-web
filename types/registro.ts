1aea78b733326c720d94f51ed8d54d93d0536230
David Cabrera <168452792+Ndav1dC@users.noreply.github.com>
2026-07-22 22:11:44 -0500

    Create RegistroForm type for user registration

---
+++ types/registro.ts
@@ -0,0 +1,12 @@
+export type RegistroForm = {
+  nombre_completo: string
+  celular: string
+  edad: string
+  barrio: string
+  fecha_nacimiento: string
+  como_conocio: string
+  invitado_por: string
+  acepto_jesus: "" | "si" | "no"
+  peticion_oracion: string
+  autorizacion_datos: boolean
+}
